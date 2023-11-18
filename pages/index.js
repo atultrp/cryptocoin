02fc5d1dfe76f3d1e6aea4e6042cc2b9d047c2a3
Atul Tripathi <atul31815@gmail.com>
2023-11-18 17:00:26 +0530

    serach bar added

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [cryptoData, setCryptoData] = useState([])
+  const [filteredData, setFilteredData] = useState([])
 
   const cryptoApiCall = async () => {
     try {
       const res = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=150&page=1&sparkline=false")
       const data = await res.json()
       setCryptoData(data)
+      setFilteredData(data)
     }
 
     catch (err) {
@@ -23,10 +25,25 @@ export default function Home() {
     cryptoApiCall()
   }, []);
 
+  const handleSearch = (searchTxt) => {
+    const tempFilteredData = cryptoData.filter((crypto) => {
+      return crypto.name.toLowerCase().includes(searchTxt.toLowerCase())
+    })
+    setFilteredData(tempFilteredData)
+  }
+
   return (
     <div>
+      <div className="flex justify-center mt-6">
+        <input
+          type="search"
+          placeholder="Search Cryptocurrency..."
+          className="w-full mx-10 md:w-1/3 rounded-md border-2 border-gray-500 px-3 py-2 focus:ring-0 focus:outline-none"
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
       <div className="mx-5 my-7 flex flex-wrap justify-center">
-        {cryptoData.map((crypto, index) => {
+        {filteredData.map((crypto, index) => {
           return (
             <Card key={index} crypto={crypto} />
           )
