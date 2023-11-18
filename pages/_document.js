a68d03f2883219c678d8b2809eea83f0b4e8940b
Atul Tripathi <atul31815@gmail.com>
2023-11-18 17:01:40 +0530

    title added

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Crypto-Coin</title>
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
       </Head>
       <body>
